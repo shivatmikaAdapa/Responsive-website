6791421f1b0061cddce5aaaff99c6867e64be866
shivatmikaAdapa <131965075+shivatmikaAdapa@users.noreply.github.com>
2024-08-24 18:24:06 +0530

    Add files via upload

---
+++ Responsive Website/script.js
@@ -0,0 +1,9 @@
+function shownav() {
+    document.querySelector('.hide').style.display = 'flex';
+    document.querySelector('.nav').style.display = 'none';
+}
+
+function hidenav() {
+    document.querySelector('.hide').style.display = 'none';
+    document.querySelector('.nav').style.display = 'flex';
+}
